0228c1bc711aacf413dec7d111e1322cf941d70f
Ben Gibson <beng@extrahop.com>
2019-11-12 19:00:25 -0500

    Update license controller

--- src/controllers/License.controller.js
+++ src/controllers/License.controller.js
@@ -1,16 +1,22 @@
 // License.controller.js
 
+const BaseCtrl = require('../controllers/_base/BaseCtrl.controller');
 const License = require('../models/license/License.model');
 
-module.exports = class LicenseCtrl {
+module.exports = class LicenseCtrl extends BaseCtrl {
 	constructor(appliance) {
-		this.appliance = appliance;
-    this.license = this.getLicense();
-
-    return new License(this.license);
+		super(appliance);
 	}
 
+  get() {
+    return new License(this.getLicense())
+  }
+
+  // -------------------------------------
+  // Base Functions
+  // -------------------------------------
+
   getLicense() {
-    return this.appliance.getLicense().data || {};
+    return this.process(this.appliance.getLicense(), 'license');
   }
 }
