0696e688afce634c22c4426d72ebaf826f7db4b3
Ben Gibson <bgibson@pm.me>
2019-11-15 11:03:49 -0500

    Fix syntax error

--- src/models/customization/CustomizationStatus.model.js
+++ src/models/customization/CustomizationStatus.model.js
@@ -4,7 +4,7 @@ const BaseObject = require('../../models/_base/BaseObject.model');
 
 module.exports = class CustomizationStatus extends BaseObject {
   constructor(customizationStatus = {}) {
-    super(customizationStatus);{
+    super(customizationStatus);
     this.did_last_succeed = customizationStatus.did_last_succeed;
     this.last_attempt_time = customizationStatus.last_attempt_time;
     this.last_success_time = customizationStatus.last_success_time;
