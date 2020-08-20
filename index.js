f72c0f936df37afc03b524b9cb6274f4e2bceba0
Ben Gibson <beng@extrahop.com>
2020-08-20 11:24:26 -0400

    Formatting tweaks

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 // index.js
 
-const Appliance = require('./src/models/_app/Appliance.model')
-const ApplianceSet = require('./src/models/_app/ApplianceSet.model')
-const Environment = require('./src/models/_app/Environment.model')
-const EnvironmentSet = require('./src/models/_app/EnvironmentSet.model')
+const Appliance = require('./src/models/_app/Appliance.model');
+const ApplianceSet = require('./src/models/_app/ApplianceSet.model');
+const Environment = require('./src/models/_app/Environment.model');
+const EnvironmentSet = require('./src/models/_app/EnvironmentSet.model');
 
 module.exports = class Extrahop {
-  constructor (config) {
+  constructor(config) {
     // ApplianceSet or EnvironmentSet
     if (config instanceof Array) {
       if (config[0].hostname && config[0].apikey) {
-        return config.length > 1 ? new ApplianceSet(...config) : new Appliance(config[0])
+        return config.length > 1 ? new ApplianceSet(...config) : new Appliance(config[0]);
       }
 
       if (config[0].name && config[0].appliances) {
-        return config.length > 1 ? new EnvironmentSet(...config) : new Environment(config[0])
+        return config.length > 1 ? new EnvironmentSet(...config) : new Environment(config[0]);
       }
     }
+
     // Appliance
     else if (config.hostname && config.apikey) {
-      return new Appliance(config)
+      return new Appliance(config);
     }
+
     // Environment
     else if (config.name && config.appliances) {
-      return new Environment(config)
+      return new Environment(config);
     }
+
     // Invalid config
     else {
-      console.error('No configuration provided or config file not found')
+      console.error('No configuration provided or config file not found');
     }
   }
-}
+};
--- src/utils/BaseUtil.util.js
+++ src/utils/BaseUtil.util.js
@@ -4,17 +4,17 @@ const crypto = require('crypto');
 
 const base = 16;
 
-const buildQuery = params => {
-  const keys = Object.keys(params || {}).filter(x => params[x] != null);
+const buildQuery = (params) => {
+  const keys = Object.keys(params || {}).filter((x) => params[x] != null);
 
   if (keys.length) {
-    return `?${keys.map(key => `${key}=${encodeURIComponent(params[key])}`).join('&')}`;
+    return `?${keys.map((key) => `${key}=${encodeURIComponent(params[key])}`).join('&')}`;
   }
 
   return null;
 };
 
-const generateId = params => {
+const generateId = (params) => {
   if (!params) {
     return Date.now().toString(base);
   }
@@ -22,6 +22,6 @@ const generateId = params => {
   return crypto.createHash('md5').update(JSON.stringify(params), 'utf-8').digest('hex');
 };
 
-const parseId = id => parseInt(id, base);
+const parseId = (id) => parseInt(id, base);
 
 module.exports = { buildQuery, generateId, parseId };
