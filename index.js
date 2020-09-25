82231770ffcec9b9520889cc4e2e2da38fb000cf
Ben Gibson <beng@extrahop.com>
2020-09-25 15:27:22 -0400

    Improved config parsing

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // index.js
 
-const Swagger = require('./src/models/_http/Swagger.model');
 const Appliance = require('./src/models/_app/Appliance.model');
 const ApplianceSet = require('./src/models/_app/ApplianceSet.model');
 const Environment = require('./src/models/_app/Environment.model');
@@ -9,36 +8,29 @@ const { Banner } = require('./src/constants/Global.constants');
 
 module.exports = class Extrahop {
   constructor(config, options = {}) {
-    // Check for quiet output mode
-    if (options.quiet) {
-      process.env.LOG_LEVEL = 'error';
+    console.log(Banner);
+
+    // ApplianceSet
+    if (Array.from(config).length && config[0].hostname && config[0].apikey) {
+      return config.length > 1 ? new ApplianceSet(config) : new Appliance(config[0]);
     }
 
-    //* console.log(Banner);
-
-    // ApplianceSet or EnvironmentSet
-    if (config instanceof Array) {
-      if (config[0].hostname && config[0].apikey) {
-        return config.length > 1 ? new ApplianceSet(...config) : new Appliance(config[0]);
-      }
-      if (config[0].name && config[0].appliances) {
-        return config.length > 1 ? new EnvironmentSet(...config) : new Environment(config[0]);
-      }
+    // EnvironmentSet
+    if (Array.from(config).length && config[0].appliances) {
+      return config.length > 1 ? new EnvironmentSet(config) : new Environment(config[0]);
     }
 
-    // Check for Appliance
-    else if (config.hostname && config.apikey) {
+    // Appliance
+    if (config.hostname && config.apikey) {
       return new Appliance(config);
     }
 
-    // Check for Environment
-    else if (config.name && config.appliances) {
+    // Environment
+    if (config.appliances) {
       return new Environment(config);
     }
 
-    // Invalid config
-    else {
-      console.error('No configuration provided or config file not found');
-    }
+    // Invalid
+    console.error('No configuration provided or config file not found');
   }
 };
