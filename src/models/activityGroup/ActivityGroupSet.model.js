a5f65251770a06fe6d7d5824e8687c4407c27149
Ben Gibson <beng@extrahop.com>
2020-09-25 14:26:16 -0400

    Fix filtering error

--- src/models/activityGroup/ActivityGroupSet.model.js
+++ src/models/activityGroup/ActivityGroupSet.model.js
@@ -5,6 +5,6 @@ const ActivityGroup = require('./ActivityGroup.model');
 
 module.exports = class ActivityGroupSet extends BaseObjectSet {
   constructor(activityGroups = []) {
-    super(Array.from(activityGroup).map((activityGroup) => new ActivityGroup(activityGroup)));
+    super(Array.from(activityGroups).map((activityGroup) => new ActivityGroup(activityGroup)));
   }
 };
